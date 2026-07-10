9633744caf21ce518536951f4a248ab526cf7b67
azkahidayat <azka.reksalegora12@gmail.com>
2026-07-10 10:28:00 +0700

    feat: add AuthPage and update auth routes

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,35 @@
+import authImage from '@/assets/images/auth-image.png';
+import Logo from '@/components/shared/Logo';
+import AuthTabs from '@/features/auth/components/AuthTabs';
+import Image from 'next/image';
+
+const AuthPage = () => {
+  return (
+    <div className='flex'>
+      <div className='hidden lg:block lg:w-1/2 h-screen overflow-hidden relative'>
+        <Image
+          src={authImage}
+          alt='burger on the tray'
+          loading='eager'
+          fill
+          sizes='(max-width: 1023px) 0vw, 50vw'
+          className='object-cover object-bottom'
+        />
+      </div>
+      <div className='flex justify-center items-center lg:w-1/2 px-3xl py-8 min-h-screen w-full'>
+        <div className='flex flex-col gap-xl lg:gap-2xl w-full max-w-93.5'>
+          <Logo />
+          <p className='font-extrabold text-display-xs lg:text-display-sm'>
+            Welcome Back
+          </p>
+          <p className='font-medium text-sm lg:text-md'>
+            Good to see you again! Let’s eat
+          </p>
+          <AuthTabs />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthPage;
